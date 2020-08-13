6f25f2988b63b21a77719812166dffa70d664bd6
Glitch (personality-quiz-starter-code) <none>
2020-08-13 14:44:36 +0000

    👲⛷ Checkpoint ./index.html:20915945/1495 ./script.js:20915945/1733

--- script.js
+++ script.js
@@ -14,28 +14,63 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
-var q2a1 = document.getElementById("q1a1");
-var q2a2 = document.getElementById("q1a2");
-var q2a3 = document.getElementById("q1a3");
-var q2a4 = document.getElementById("q1a4");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
 
-var q3a1 = document.getElementById("q1a1");
-var q3a2 = document.getElementById("q1a2");
-var q3a3 = document.getElementById("q1a3");
-var q3a4 = document.getElementById("q1a4");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
 
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", result1);
+q1a1.addEventListener("click", spiderman);
+q2a1.addEventListener("click", spiderman);
+q3a1.addEventListener("click", spiderman);
+q4a1.addEventListener("click", spiderman);
 
+q1a2.addEventListener("click", blackWidow);
+q2a2.addEventListener("click", blackWidow);
+q3a2.addEventListener("click", blackWidow);
+q4a2.addEventListener("click", blackWidow);
 
+q1a3.addEventListener("click", starLord);
+q2a3.addEventListener("click", starLord);
+q3a3.addEventListener("click", starLord);
+q4a3.addEventListener("click", starLord);
 
-
-
+q1a4.addEventListener("click", buzzLightyear);
+q2a4.addEventListener("click", buzzLightyear);
+q3a4.addEventListener("click", buzzLightyear);
+q4a4.addEventListener("click", buzzLightyear);
 
 
 //#TODO: Define quiz functions here
-function result1(){
+function spiderman(){
+  
+  
+  
+}
+
+function blackWidow(){
+  
+  
+  
+}
+
+function starLord(){
+  
+  
+  
+}
+
+function buzzLightyear(){
   
   
   
